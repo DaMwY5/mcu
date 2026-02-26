29ce0a227ac21b6d8d7e9dd7e40e38a11f1afd2a
DaMwY <shar.dam655v@gmail.com>
2026-02-26 14:28:00 +0300

    a

--- 02-control/main.c
+++ 02-control/main.c
@@ -1,13 +1,37 @@
-
-#include "stdlib.h"
-#include "pico/stdlib.h"
-#include "stdio.h"
-#include "hardware/gpio.h"
 #include "stdio-task/stdio-task.h"
-int main(){
+#include "pico/stdlib.h"
+#include "protocol-task/protocol-task.h"
+#include "stdio.h"
+
+#define DEVICE_NAME "my-pico-device"
+#define DEVICE_VRSN "v0.0.1"
+
+void version_callback(const char *args)
+{
+    printf("device name: '%s', firmware version: %s\n", DEVICE_NAME, DEVICE_VRSN);
+}
+
+
+api_t device_api[] =
+    {
+        {"version", version_callback, "get device name and firmware version"},
+        {NULL, NULL, NULL},
+};
+
+int main()
+{
     stdio_init_all();
+
     stdio_task_init();
-    while(1){
-        stdio_task_handle();
+
+    protocol_task_init(device_api);
+
+
+    while (1)
+    {
+        // stdio_task_handle();
+
+        protocol_task_handle(stdio_task_handle());
+
     }
 }
